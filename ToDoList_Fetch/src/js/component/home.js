c6cb5b7b8be70582624e3bd8771bd5f98dc7fce3
eddiejosue14 <43359575+eddiejosue14@users.noreply.github.com>
2021-05-28 19:05:58 -0600

    Correcccion
    Correcccion

--- ToDoList_Fetch/src/js/component/home.js
+++ ToDoList_Fetch/src/js/component/home.js
@@ -1,25 +1,153 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
-export function Home()
- {
+export function Home() {
+	const [task, setTask] = useState([]);
+
+	let url = "https://assets.breatheco.de/apis/fake/todos/user/KattiaSM";
+
+	function getTodo() {
+		fetch(url, {
+			method: "GET",
+			//body:
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(res => res.json())
+			.then(data => {
+				setTask(data); //Setea mi lista :  [{ label: "Make the bed", done: false }]
+				console.log({ data });
+				console.log(`data from get<todo`, data);
+			})
+			.catch(error => console.log("Error:", error.message));
+	}
+	function newTodo() {
+		//adicionar fetch
+		//let array = []
+		//method POST
+		//body: JSON.stringify(array)
+		let array = [];
+		fetch(url, {
+			method: "POST",
+			body: JSON.stringify(array),
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(res => res.json())
+			.then(data => {
+				console.log(`data from get<todo`, data);
+				getTodo();
+			})
+			.catch(error => console.log("Error:", error.message));
+	}
+
+	function updateTodo(task) {
+		//adicionar fetch
+		//method PUT
+		//body: JSON.stringify(task)
+		fetch(url, {
+			method: "PUT",
+			body: JSON.stringify(task),
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(res => res.json())
+			.then(data => {
+				getTodo();
+				alert(data.result);
+			})
+			.catch(error => console.log("Error:", error.message));
+	}
+
+	function deleteTodo() {
+		//adicionar fetch
+		//method DELETE
+		//despues de deletear llamar al metodo post como indica el api
+		fetch(url, {
+			method: "DELETE",
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(res => res.json())
+			.then(data => {
+				newTodo();
+				alert(data.result);
+			})
+			.catch(error => console.log("Error:", error.message));
+	}
+
+	function addItem(e) {
+		if (e.key === "Enter" && e.target.value !== "") {
+			console.log(`task`, task);
+			let obj = { label: e.target.value, done: true };
+			setTask(task.concat(obj));
+			//newTodo([{ label: e.target.value, done: false }]);
+			//getTodo();
+			e.target.value = "";
+		}
+	}
+
+	function deleteItem(index) {
+		if (index > -1) {
+			const filterData = task.filter(item => item !== task[index]);
+			setTask(filterData);
+		}
+	}
+
+	useEffect(() => {
+		getTodo();
+	}, []);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="title">To Do List</h1>
+			<div className="inputValue">
+				<input
+					type="text"
+					className="form-control" // bootstrap
+					aria-label="Sizing example input" // bootstrap
+					aria-describedby="inputGroup-sizing-default" // bootstrap
+					name="item"
+					placeholder="Add the items here"
+					onKeyPress={e => addItem(e)}></input>
+				<div className="list">
+					<ul className="list-group">
+						{task.map((item, index) => {
+							return (
+								<li
+									key={index} // cuando uno hace un map dentro de un jsx y vas a mapear un elemento del DOM, a dicho elemento tienes que agregarle una key única. Sino, da problemas para encontrar la key
+									className=" list-group-item d-flex justify-content-between align-items-center"
+									onClick={() => {
+										deleteItem(index);
+									}}>
+									{item.label}
+									<span>
+										<i className="fas fa-trash"></i>
+									</span>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
+			<div className="row d-flex justify-content-center">
+				<button
+					type="button"
+					className="btn btn-danger"
+					onClick={() => {
+						deleteTodo();
+					}}>
+					Delete List
+				</button>
+				<button
+					type="button"
+					className="btn btn-success"
+					onClick={() => {
+						updateTodo(task);
+					}}>
+					Update List
+				</button>
+			</div>
+
+			<div className="counter">
+				<p>{task.length} Cantidad de tareas</p>
+			</div>
 		</div>
 	);
 }
--- ToDoList_Fetch/src/js/index.js
+++ ToDoList_Fetch/src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Todolist } from "./component/todolist.js";
+import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Todolist />, document.querySelector("#app"));
+ReactDOM.render(<Home />, document.querySelector("#app"));
